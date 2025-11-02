9e2f9633203e96aa7245846df2805c383983e6ec
An Huynh <96190092+annguyenhuynh@users.noreply.github.com>
2025-11-02 15:48:23 -0500

    Delete ec2.tf

--- ec2.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "example" {
-    ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-    instance_type = "t3.micro"
-    subnet_id     = "subnet-02ca7f775f2785f08"
-    key_name      = "ProwlerTest"
-}
