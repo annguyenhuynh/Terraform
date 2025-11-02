1ef5494e070eda9f0ef3059110743d09f87a2b3e
An Huynh <ahuynh@credence-llc.com>
2025-11-02 16:18:56 -0500

    added new resource

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+ami_id              = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+instance_type       = "t3.micro"
+subnet_id           = "subnet-092fd5f6c280281ec"
+security_group_id   = "sg-070550909ef42358b"
+aws_region          = "us-gov-west-1"
+key_name            = "ProwlerTest"
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "ami_id" {
+    description = "AMI ID for the EC2 instance"
+    type        = string
+}
+
+variable "instance_type" {
+    description = "Type of EC2 instance"
+    type        = string  
+}
+
+variable "subnet_id" {
+    description = "Subnet ID for the EC2 instance"
+    type        = string
+}
+
+variable "security_group_id" {
+    description = "value of security group ID"
+    type        = string
+}
+
+variable "key_name" {
+    description = "Key pair name for SSH access"
+    type        = string
+}
+
+variable "aws_region" {
+    description = "AWS region to deploy resources"
+    type        = string
+  
+}
