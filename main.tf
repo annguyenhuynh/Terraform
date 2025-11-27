ad0593232af55dc82734ccf0d0e8da94e2f3d978
An Huynh <ahuynh@credence-llc.com>
2025-11-27 13:55:50 -0500

    redeploy terraform resources

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
 provider "aws" {
     region = var.aws_region 
+    profile = "default"
 }
 
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket        = "an-terraform-tfstate"
+  force_destroy = true
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "an-terraform-tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 module "extra_ec2" {
     source        = "./modules/ec2"
     ami_id        = var.ami_id  
@@ -10,7 +26,5 @@ module "extra_ec2" {
     key_name      = var.key_name
     name          = "dev-instance-${terraform.workspace}"
     security_group_id = var.security_group_id
-    
-    
 }
 
